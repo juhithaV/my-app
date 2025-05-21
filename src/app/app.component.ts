4afd597691e6c65d2cb43e782a35b2978a26dd33
juhithaV <juhithavemuri@gmail.com>
2025-05-22 00:51:28 +0530

    final to-do app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -6,25 +6,34 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'to-do';
   taskForm = new FormGroup({
     task: new FormControl('',Validators.required)
   })
   data:{task:string,completed:boolean}[]=[];
+  containsData:boolean  = false;
 
+  ngOnInit() {
+    if(this.data.length != 0){
+      this.containsData = true;
+    }
+  }
   onSubmit(){
     const newTask = this.taskForm.value.task;
     if (newTask){
       this.data.push({task: newTask,completed: false});
       this.taskForm.reset();
+      this.containsData = true;
     }
   }
   updateTask(index:number){
     this.data[index].completed = true;
   }
   deleteTask(index:number){
-    console.log("Hi",index)
     this.data.splice(index,1);
+    if(this.data.length == 0){
+      this.containsData = false;
+    }
   }
 }
